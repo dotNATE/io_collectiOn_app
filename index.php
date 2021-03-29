5bc11c2387dacaf4bb3a37bdf1ca3fee727a0d7a
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-03-29 16:18:38 +0100

    added tests for story1

--- index.php
+++ index.php
@@ -1,12 +1,31 @@
-<?php require 'functions.php'; ?>
+<?php
+
+$db = new PDO("mysql:host=db; dbname=collection_app", "root", "password");
+$db ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+require 'functions.php';
+
+$query = getAllFromDB($db);
+$output = prepareOutput($query);
+
+?>
+
+    <h2>NEW BOOK?</h2>
+
+<form method="post">
+    <label>Book Title: <input type="text" name="book_title"/></label><br>
+    <label>Author: <input type="text" name="author"/></label><br>
+    <label>Released: <input type="number" name="year_released"/></label><br>
+    <label>Genre: <input type="text" name="genre"/></label><br>
+    <label>Page Count: <input type="number" name="page_count"/></label><br>
+    <input type="submit" value="Add to collection" />
+</form>
 
 <h2>BOOK COLLECTION</h2>
 
 <?php
 
-$query = getAllFromDB($db);
-$output = prepareOutput($query);
-
+// foreach statement outputs all db entries to HTML in formatted state
 foreach ($output as $key => $value) {
     echo $value;
 }
---
+++ tests/functions.php
@@ -0,0 +1,27 @@
+<?php
+
+require '../functions.php';
+
+use PHPUnit\Framework\TestCase;
+
+class functions extends TestCase
+{
+    public function testPrepareOutput_success1()
+    {
+        $result = prepareOutput([['id' => 'idtest', 'book_title' => 'title', 'author' => 'author', 'genre' => 'genre', 'page_count' => 'pages', 'year_released' => 'year']]);
+        $this->assertisArray($result);
+    }
+
+    public function testPrepareOutput_success2()
+    {
+        $result = prepareOutput([['id' => 'idtest', 'book_title' => 'title', 'author' => 'author', 'genre' => 'genre', 'page_count' => 'pages', 'year_released' => 'year']]);
+        $expected = [['<pre><span class="class"><h2>title</h2><p><em>author</em></p><br>Released in year. genre. pages pages.</span></pre>']];
+        $this->assertEquals($result, $expected[0]);
+    }
+
+    public function testPrepareOutput_malformed1()
+    {
+        $this->expectException(TypeError::class);
+        prepareOutput(100);
+    }
+}
