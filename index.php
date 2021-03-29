0a14cd832df1572cdb4badb3d7bb7381c24600b0
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-03-29 16:19:28 +0100

    added tests for story1

--- index.php
+++ index.php
@@ -12,15 +12,6 @@ $output = prepareOutput($query);
 
     <h2>NEW BOOK?</h2>
 
-<form method="post">
-    <label>Book Title: <input type="text" name="book_title"/></label><br>
-    <label>Author: <input type="text" name="author"/></label><br>
-    <label>Released: <input type="number" name="year_released"/></label><br>
-    <label>Genre: <input type="text" name="genre"/></label><br>
-    <label>Page Count: <input type="number" name="page_count"/></label><br>
-    <input type="submit" value="Add to collection" />
-</form>
-
 <h2>BOOK COLLECTION</h2>
 
 <?php
