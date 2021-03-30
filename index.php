358ed45fdf69c909fc9810fff7037ec470b5b184
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-03-30 16:03:41 +0100

    added stylesheet, removed <br>'s

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
     <head lang="en-GB">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css" />
         <title>Collect-iO-n Application</title>
     </head>
     <body>
@@ -25,11 +26,11 @@
     ?>
 
     <form method="post" action="form.php">
-        <label>Title: <input type="text" name="title" placeholder="required" /></label><br>
-        <label>Author: <input type="text" name="author" placeholder="required" /></label><br>
-        <label>Year Released: <input type="number" name="released" placeholder="required" /></label><br>
-        <label>Genre: <input type="text" name="genre" placeholder="required" /></label><br>
-        <label>Page Count: <input type="number" name="page_count" placeholder="required" /></label><br>
+        <label>Title: <input type="text" name="title" placeholder="required" /></label>
+        <label>Author: <input type="text" name="author" placeholder="required" /></label>
+        <label>Year Released: <input type="number" name="released" placeholder="required" /></label>
+        <label>Genre: <input type="text" name="genre" placeholder="required" /></label>
+        <label>Page Count: <input type="number" name="page_count" placeholder="required" /></label>
         <input type="submit" value="Add to Collection"/>
     </form>
 
